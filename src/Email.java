dbfea1ade0e51d291d2922de8db27c027537cd41
stealthscripter <nicolasayele@gmail.com>
2023-12-26 18:17:59 +0300

    init

--- src/Email.java
+++ src/Email.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Email {
     private String firstname;
     private String lastname;
@@ -10,9 +13,28 @@ public class Email {
         this.firstname = firstname;
         this.lastname = lastname;
 
-        System.out.println("Firstname: " + "\n" +  "Lastname: " + "\n");
+        this.department = setDepartment();
+        System.out.println("Firstname: "+ firstname + "\n" +  "Lastname: "+lastname + "\n" +  "Department: "+department + "\n");
+
     }
-    public void display(){
-        System.out.println("FirstName: " + firstname + "\n" + "LastName: " + lastname);
+    private String setDepartment(){
+        System.out.println("Enter the department\n 1 For Sale \n 2 For Development \n 3 For Accounting \n 0 For none ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if(depChoice == 1) {
+            return  "Sales";
+        } else if (depChoice == 2) {
+            return "Dev";
+        } else if (depChoice == 3) {
+            return "Acct";
+        }
+        else {
+            return  " ";
+        }
+    }
+    private String setPassword(){
+
+        String randpass = Random()
+        return
     }
 }
